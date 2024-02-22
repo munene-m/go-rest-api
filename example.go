7cb00a958f6591e3dcf119b97fd7cf550e9d921c
munene-m <munenenjue18@gmail.com>
2024-02-22 23:48:44 +0300

    chore: added put and delete endpoints and functions. The api is now fully CRUD

--- example.go
+++ example.go
@@ -14,7 +14,7 @@ import (
 )
 
 // album represents data about a record album.
-type album struct {
+type Album struct {
     ID     int64  `json:"id"`
     Title  string  `json:"title"`
     Artist string  `json:"artist"`
@@ -22,7 +22,7 @@ type album struct {
     Created *time.Time `json:"created,omitempty"`
 }
 
-var dataMap = make(map[string]string)
+// var dataMap = make(map[string]string)
 
 func main(){
     err := godotenv.Load(".env")
@@ -56,9 +56,27 @@ func main(){
             getAlbumByID(c, db)
         }
     }
+    getAlbumsHandler := func(db *sql.DB) gin.HandlerFunc {
+        return func(c *gin.Context){
+            getAlbums(c, db)
+        }
+    }
+    updateAlbumHandler := func(db *sql.DB) gin.HandlerFunc {
+        return func(c *gin.Context){
+            updateAlbum(c, db)
+        }
+    }
+    deleteAlbumHandler := func(db *sql.DB) gin.HandlerFunc{
+        return func(c *gin.Context){
+            deleteAlbum(c, db)
+        }
+    }
 
 	r.GET("/albums/:id", getAlbumHandler(db))
+    r.GET("/albums", getAlbumsHandler(db))
 	r.POST("/albums", postAlbumsHandler(db) )
+    r.PUT("/albums/update/:id", updateAlbumHandler(db))
+    r.DELETE("/albums/delete/:id", deleteAlbumHandler((db)))
 
 	r.Run(":8080") //listen and serve on 0.0.0.0:8000 
 }
@@ -68,7 +86,7 @@ func createAlbumStore(db *sql.DB){
         id SERIAL PRIMARY KEY,
         Title VARCHAR(100) NOT NULL,
         Artist VARCHAR(100) NOT NULL,
-        Price NUMERIC(6,2) NOT NULL,
+        Price NUMERIC(10,2) NOT NULL, 
         Created timestamp DEFAULT NOW()
     )`
     _, err := db.Exec(query)
@@ -78,7 +96,7 @@ func createAlbumStore(db *sql.DB){
 }
 
 func postAlbums(c *gin.Context,  db *sql.DB){
-    var newAlbum album
+    var newAlbum Album
     if err := c.BindJSON(&newAlbum); err != nil {
         return
     }
@@ -108,11 +126,59 @@ func postAlbums(c *gin.Context,  db *sql.DB){
     }
     c.IndentedJSON(http.StatusCreated, gin.H{"message": "Album created successfully","id":pk})
 }
+
+func updateAlbum(c *gin.Context, db *sql.DB){
+    id := c.Param("id")
+    row := db.QueryRow("SELECT * FROM albumStore WHERE id = $1", id)
+
+    var album Album
+    if err := row.Scan(&album.ID,&album.Title,&album.Artist, &album.Price,  &album.Created); err != nil{
+        if err == sql.ErrNoRows{
+            c.JSON(http.StatusNotFound, gin.H{"message":"Album not found."})
+        }
+        //any other error
+        log.Fatal(err)
+        c.JSON(http.StatusInternalServerError, gin.H{"message":"Internal server error"})
+        return
+    }
+
+     // Bind JSON payload to updatedAlbum
+     var updatedAlbum Album
+     if err := c.BindJSON(&updatedAlbum); err != nil {
+         c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+         return
+     }
+ 
+     // Update corresponding fields of existing album with values from updatedAlbum
+     if updatedAlbum.Title != "" {
+         album.Title = updatedAlbum.Title
+     }
+     if updatedAlbum.Artist != "" {
+         album.Artist = updatedAlbum.Artist
+     }
+     if updatedAlbum.Price != 0 {
+         album.Price = updatedAlbum.Price
+     }
+
+      // Execute SQL UPDATE statement to update the album record in the database
+    _, err := db.Exec("UPDATE albumStore SET title = $1, artist = $2, price = $3 WHERE id = $4",
+    album.Title, album.Artist, album.Price, album.ID)
+if err != nil {
+    log.Fatal(err)
+    c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+    return
+}
+
+// Return the updated album in the response
+c.JSON(http.StatusOK, album)
+
+}
+
 func getAlbumByID(c *gin.Context, db *sql.DB) {
     id := c.Param("id")
 
     // Query the database to find the album with the specified ID.
-    var album album
+    var album Album
     query := "SELECT * FROM albumStore WHERE id = $1"
     err := db.QueryRow(query, id).Scan(&album.ID, &album.Title, &album.Artist, &album.Price, &album.Created)
     if err != nil {
@@ -125,4 +191,55 @@ func getAlbumByID(c *gin.Context, db *sql.DB) {
     }
 
     c.JSON(http.StatusOK, album)
+}
+func getAlbums(c *gin.Context, db *sql.DB){
+    data := []Album{}
+    rows, err := db.Query("SELECT title, artist, price, id FROM albumStore")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+    var title string
+    var artist string
+    var price float64
+    var id int64
+
+    for rows.Next(){
+        err := rows.Scan(&title, &artist, &price, &id)
+        if err != nil {
+            log.Fatal(err)
+        }
+        data = append(data, Album{Title: title, Artist: artist, Price: price, ID: int64(id)})
+    }
+    c.JSON(http.StatusOK, data)
+}
+
+func deleteAlbum(c *gin.Context, db *sql.DB){
+    id := c.Param("id")
+
+    var exists bool
+    err := db.QueryRow("SELECT EXISTS (SELECT 1 FROM albumStore WHERE id = $1)", id).Scan(&exists)
+    if err != nil {
+        // Log the error message
+        log.Println("Error checking album existence:", err)
+        // Return an error response to the client
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check album existence"})
+        return
+    }
+    
+    if !exists {
+        // Album does not exist, return a 404 Not Found response
+        c.JSON(http.StatusNotFound, gin.H{"error": "Album not found"})
+        return
+    }
+    
+    query := `DELETE FROM albumStore WHERE id = $1`
+    _ , deletionErr := db.Exec(query, id)
+    if deletionErr != nil {
+        log.Println("Error deleting album:", deletionErr)
+        // Return an error response to the client
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to delete album"})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"message": "Album deleted successfully"})
 }
